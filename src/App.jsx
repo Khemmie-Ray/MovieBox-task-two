d1d5895ea1a539b1e4c252a4fc51d6c3df3b8cd0
Khemmie-Ray <atokemmy@gmail.com>
2023-09-14 15:01:12 +0100

    updates to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,83 @@
-import { useState } from 'react'
-import './App.css'
-import posterBg from './assets/Poster.svg'
-import logo from './assets/Logo.svg'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import posterBg from './assets/Poster.svg';
+import logo from './assets/Logo.svg';
 
 function App() {
-  const [movieData, setMovieData] = useState([])
+  const [movieData, setMovieData] = useState([]);
 
-  const options = {
-    method: 'GET',
-      cache: "force-cache",
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzViNDE4ZTcyMGI0NDk0ZDdkMDQ2OWExY2M4ZjZmZSIsInN1YiI6IjY1MDA5NGMwNTU0NWNhMDBmZWE2YjMwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JHVLVRgITnuDtg9BlNFy7Aawslr5goZjUGc3K5lk1p0'
-    }
-  };
-  
-  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      setMovieData(data)
-    })
-    .catch(err => console.error(err));
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
+          {
+            method: 'GET',
+            cache: 'force-cache',
+            headers: {
+              accept: 'application/json',
+              Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzViNDE4ZTcyMGI0NDk0ZDdkMDQ2OWExY2M4ZjZmZSIsInN1YiI6IjY1MDA5NGMwNTU0NWNhMDBmZWE2YjMwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JHVLVRgITnuDtg9BlNFy7Aawslr5goZjUGc3K5lk1p0',
+            },
+          }
+        );
+        const data = await response.json();
+        console.log(data.results);
+        setMovieData(data.results);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const first10Movies = movieData.slice(0, 10);
+
+  const MovieCard = first10Movies.map((info) => (
+    <div key={info.id} className='card'>
+      <img src={`https://image.tmdb.org/t/p/w500/${info.poster_path}`} alt="" />
+      <p>{}</p>
+      <h2>{info.title}</h2>
+    </div>
+  ));
 
   return (
     <>
-    <section style={{ backgroundImage: `url(${posterBg})` }} className='heroBg'>
-      <header>
-        <img src={logo} alt="" />
-        <input type="text" placeholder='search' />
-        <div>
-          <a href="Sign In"></a>
-          <a href=""><i class="ri-menu-line"></i></a>
+      <section style={{ backgroundImage: `url(${posterBg})` }} className="heroBg">
+        <header>
+          <img src={logo} alt="" />
+          <input type="text" placeholder="search" />
+          <div>
+            <a href="Sign In">Sign In</a>
+            <a href="#"><i className="ri-menu-line menu"></i></a>
+          </div>
+        </header>
+        <div className="content">
+          <h1>John Wick 3 : Parabellum</h1>
+          <p>John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.</p>
+          <button>Watch Trailer <i className="ri-play-circle-fill"></i></button>
         </div>
-      </header>
-      <div className="content">
-        <h1>John Wick 3 : Parabellum</h1>
-        <p>John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.</p>
-        <button>Watch Trailer<i class="ri-play-circle-fill"></i></button>
-      </div>
-    </section>
-      
-      
+      </section>
+      <section className="card-container">
+        {MovieCard}
+      </section>
+      <footer>
+        <div className="socials">
+          <i className="ri-facebook-circle-fill"></i>
+          <i className="ri-instagram-fill"></i>
+          <i className="ri-twitter-x-fill"></i>
+          <i className="ri-youtube-fill"></i>
+        </div>
+        <div>
+          <a href="#">Condition of Use</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Press Room</a>
+        </div>
+        <p>&copy; 2023 MovieBox by Khemmie-Ray</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
