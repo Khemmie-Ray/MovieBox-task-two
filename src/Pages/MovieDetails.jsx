dc4026a546e196618d229b2afd370849083b4053
Khemmie-Ray <atokemmy@gmail.com>
2023-11-17 18:36:15 +0100

    updates to details page

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import logo from '../assets/movieLogo.svg';
+import posterBg from '../assets/moviebg.svg';
 
 const MovieDetails = () => {
     const { movieId } = useParams();
@@ -27,7 +28,7 @@ const MovieDetails = () => {
         movieGenres = movieDetails.genres.map((info, index) => (
           <span key={info.id}>
           {info.name}
-          {index < info.name.length - 1 && ' '}
+          {index < info.name.length - 1 && ' . '}
       </span>
         ));
     }
@@ -39,14 +40,17 @@ const MovieDetails = () => {
                 <img
                     src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`}
                     alt=""
+                    className='poster-img'
                 />
+                <div className='subtitle'>
                 <h2>
                     {movieDetails.title} . {movieDetails.release_date} .{' '}
                     {movieDetails.runtime} mins
                 </h2>
-                {movieGenres && <div>{movieGenres}</div>}
-                <div>
-                  <p><i class="ri-star-fill"></i> <span>{movieDetails && movieDetails.vote_average !== undefined ? movieDetails.vote_average.toFixed(1) : 'N/A'}</span> | {movieDetails && movieDetails.popularity !== undefined ? movieDetails.popularity.toFixed() : 'N/A'}</p>
+                {movieGenres && <div className='genre'>{movieGenres}</div>}
+                <div className='rating'>
+                  <p><i class="ri-star-fill"></i> <span className='vote'>{movieDetails && movieDetails.vote_average !== undefined ? movieDetails.vote_average.toFixed(1) : 'N/A'}</span> | {movieDetails && movieDetails.popularity !== undefined ? movieDetails.popularity.toFixed() : 'N/A'}k</p>
+                </div>
                 </div>
             </div>
         );
@@ -55,23 +59,40 @@ const MovieDetails = () => {
   console.log(movieDetails)
 
     return (
-      <section>
+      <section className='details-container'>
         <div className="sidebar">
-          <img src={logo} alt="" />
-          <ul>
+          <nav>
+          <img src={logo} alt="" className='side-logo'/>
+          <i className="ri-menu-2-line hamburger"></i>
+          </nav>
+          <ul className='navlinks'>
             <li><a href="#"><i className="ri-home-4-fill"></i>Home</a></li>
-            <li><a href=""><i className="ri-vidicon-fill">Movies</i></a></li>
-            <li><a href=""><i className="ri-tv-fill"></i>TV Series</a></li>
-            <li><a href=""><i className="ri-calendar-todo-fill"></i>Upcoming</a></li>
+            <li className='active'><a href="#"><i className="ri-vidicon-fill"></i>Movies</a></li>
+            <li><a href="#"><i className="ri-tv-fill"></i>TV Series</a></li>
+            <li><a href="#"><i className="ri-calendar-todo-fill"></i>Upcoming</a></li>
           </ul>
-          <div>
+          <div className='quiz-card'>
            <h3>Play movie quizes and earn free tickets</h3>
            <p>50k people are playing now</p>
            <button>Start playing</button>
           </div>
         </div>
-        <div>
+        <div className='details-card'>
           {details}
+          <section className='overview-card'>
+            <div className='overview'>
+          {movieDetails && (
+            <div>
+              <p>{movieDetails.overview}</p>
+            </div>
+          )}
+            </div>
+            <div className='options-card'>
+              <button className='btn-showtime'><i class="ri-coupon-line"></i> See Showtimes</button>
+              <button className='btn-option'><i className="ri-list-check"></i> Watch more options</button>
+              <img src={posterBg} alt="" className='poster-bg'/>
+            </div>
+            </section>
         </div>
       </section>
     );
